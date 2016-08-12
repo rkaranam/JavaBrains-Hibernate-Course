5297b552fb7eb221c410d30c46a62594e60c4ae9
Rajasekhar Karanam <karanam.rajasekhar44@gmail.com>
2016-08-12 21:56:45 +0530

    Cleaning up environment for Hibernate mappings

--- src/org/javabrains/koushik/dto/UserDetails.java
+++ src/org/javabrains/koushik/dto/UserDetails.java
@@ -1,27 +1,15 @@
 package org.javabrains.koushik.dto;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Date;
 
-import javax.persistence.AttributeOverride;
-import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 
-import org.hibernate.annotations.CollectionId;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Type;
-import org.javabrains.koushik.valueobjects.Address;
 import org.javabrains.koushik.valueobjects.Credentials;
-import org.javabrains.koushik.valueobjects.Phone;
 
 @Entity(name = "USER_DETAILS")
 public class UserDetails {
@@ -46,33 +34,6 @@ public class UserDetails {
 	@Column(name = "DESCRIPTION")
 	private String description;
 	
-	@AttributeOverrides({
-			@AttributeOverride(name = "street", column = @Column(name = "HOME_STREET_NAME")),
-			@AttributeOverride(name = "city", column = @Column(name = "HOME_CITY_NAME")),
-			@AttributeOverride(name = "state", column = @Column(name = "HOME_STATE_NAME")),
-			@AttributeOverride(name = "pincode", column = @Column(name = "HOME_PIN_CODE") )
-	})
-	private Address homeAddress;
-	
-	@AttributeOverrides({
-			@AttributeOverride(name = "street", column = @Column(name = "OFFICE_STREET_NAME")),
-			@AttributeOverride(name = "city", column = @Column(name = "OFFICE_CITY_NAME")),
-			@AttributeOverride(name = "state", column = @Column(name = "OFFICE_STATE_NAME")),
-			@AttributeOverride(name = "pincode", column = @Column(name = "OFFICE_PIN_CODE"))			
-	})
-	private Address officeAddress;
-	
-	@ElementCollection
-	@JoinTable(
-		name = "USER_PHONE_NUMBERS",
-		joinColumns = {
-				@JoinColumn(name = "USER_LOGIN_ID"),
-				@JoinColumn(name = "USER_LOGIN_PASSWORD")
-		})
-	@GenericGenerator(name = "sequence-gen", strategy = "sequence")
-	@CollectionId(columns = { @Column(name = "ADDRESS_ID") }, generator = "sequence-gen", type = @Type(type = "long"))
-	private Collection<Phone> phoneNumbers = new ArrayList<Phone>();
-	
 	/*public UserDetails() {
 		System.out.println("UserDetails: Default Constructor called!");
 	}*/
@@ -81,36 +42,12 @@ public class UserDetails {
 		return credentials;
 	}
 
-	public Collection<Phone> getPhoneNumbers() {
-		return phoneNumbers;
-	}
-
-	public void setPhoneNumbers(Collection<Phone> phoneNumbers) {
-		this.phoneNumbers = phoneNumbers;
-	}	
-
 	public void setCredentials(Credentials credentials) {
 		this.credentials = credentials;
 	}
 	
 	public Date getJoinedTime() {
 		return joinedTime;
-	}
-
-	public Address getHomeAddress() {
-		return homeAddress;
-	}
-
-	public void setHomeAddress(Address homeAddress) {
-		this.homeAddress = homeAddress;
-	}
-
-	public Address getOfficeAddress() {
-		return officeAddress;
-	}
-
-	public void setOfficeAddress(Address officeAddress) {
-		this.officeAddress = officeAddress;
 	}
 
 	public void setJoinedTime(Date joinedTime) {
@@ -141,15 +78,6 @@ public class UserDetails {
 		this.joinedDate = joinedDate;
 	}
 
-	/*
-	public int getUserId() {
-		return userId;
-	}
-
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}*/
-
 	public String getUserName() {
 		return userName;
 	}
@@ -163,9 +91,7 @@ public class UserDetails {
 		return "UserDetails [credentials=" + credentials + ", userName="
 				+ userName + ", joinedDate=" + joinedDate + ", joinedTime="
 				+ joinedTime + ", userType=" + userType + ", description="
-				+ description + ", homeAddress=" + homeAddress
-				+ ", officeAddress=" + officeAddress + ", phoneNumbers="
-				+ phoneNumbers + "]";
+				+ description + "]";
 	}
 
 }
--- src/org/javabrains/koushik/test/HibernateTest.java
+++ src/org/javabrains/koushik/test/HibernateTest.java
@@ -1,17 +1,12 @@
 package org.javabrains.koushik.test;
 
-import java.util.Collection;
 import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.javabrains.koushik.dto.UserDetails;
-import org.javabrains.koushik.valueobjects.Address;
 import org.javabrains.koushik.valueobjects.Credentials;
-import org.javabrains.koushik.valueobjects.Phone;
 
 public class HibernateTest {
 
@@ -29,29 +24,6 @@ public class HibernateTest {
 		user1.setJoinedTime(new Date());
 		user1.setDescription("Skilled in Spring and Hibernate");
 		user1.setUserType("Permanent");
-		// user1.setAddress("Wall Street, California");
-		// user1.setAddress(new Address("Long Road", "Bohemia", "Prague", "110181"));
-		
-		Set<Phone> phonesAvailable = new HashSet<Phone>();
-		phonesAvailable.add(new Phone("789789", "Rotocar", "Blackwood County", "Home"));
-		phonesAvailable.add(new Phone("123456", "Verysol", "Malthora", "Work"));
-		user1.setPhoneNumbers(phonesAvailable);
-		
-		Address user1HomeAddr = new Address();
-		user1HomeAddr.setStreet("Sea Rd");
-		user1HomeAddr.setCity("Bohemia");
-		user1HomeAddr.setState("Prague");
-		user1HomeAddr.setPincode("1101");
-		
-		user1.setHomeAddress(user1HomeAddr);
-		
-		Address user1OfficeAddr = new Address();
-		user1OfficeAddr.setStreet("Authur Rd");
-		user1OfficeAddr.setCity("Bohemia");
-		user1OfficeAddr.setState("Prague");
-		user1OfficeAddr.setPincode("1106");
-		
-		user1.setOfficeAddress(user1OfficeAddr);
 		
 		UserDetails user2 = new UserDetails();
 		
@@ -65,30 +37,6 @@ public class HibernateTest {
 		user2.setJoinedTime(new Date());
 		user2.setUserType("Contract");
 		user2.setDescription("Hands on experience with multiple Javascript frameworks");
-		// user2.setAddress("Beijing, China");
-		// user2.setAddress(new Address("Race Court Rd", "San Jose", "California", "1022"));
-		
-		phonesAvailable = null;
-		phonesAvailable = new HashSet<Phone>();
-		phonesAvailable.add(new Phone("110234", "Vodafone", "San Jose", "Home"));
-		phonesAvailable.add(new Phone("234566", "Verizon", "Oakmont", "Work"));
-		user2.setPhoneNumbers(phonesAvailable);
-		
-		Address user2HomeAddr = new Address();
-		user2HomeAddr.setStreet("Race Court Rd");
-		user2HomeAddr.setCity("Pennylsvania");
-		user2HomeAddr.setState("California");
-		user2HomeAddr.setPincode("1022");
-		
-		user2.setHomeAddress(user2HomeAddr);
-		
-		Address user2OfficeAddr = new Address();
-		user2OfficeAddr.setStreet("Cotton Rd");
-		user2OfficeAddr.setCity("Oak City");
-		user2OfficeAddr.setState("Yorkshire");
-		user2OfficeAddr.setPincode("1044");
-		
-		user2.setOfficeAddress(user2OfficeAddr);
 		
 		SessionFactory factory = new Configuration().configure().buildSessionFactory();
 		Session session = factory.openSession();
@@ -102,18 +50,13 @@ public class HibernateTest {
 		session = factory.openSession();
 		session.beginTransaction();
 		retrievedUser = session.get(UserDetails.class, user2.getCredentials());
-		Collection<Phone> phoneNumbers = retrievedUser.getPhoneNumbers();
+
 		session.getTransaction().commit();
 		
 		System.out.println("Retrieved User object: " + retrievedUser);
+
+		session.close();		
 		
-		System.out.println("Phone Numbers of the user: " + phoneNumbers.size());
-		session.close();
-		
-		System.out.println("The Phone Numbers are: " + phoneNumbers);
-		for (Phone phone : phoneNumbers) {
-			System.out.println(phone);
-		}
 		session.getSessionFactory().close();
 		
 	}
